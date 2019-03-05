dc2df91e478bd494530330d4ebdd69b20bc562b3
viq <vicviq@gmail.com>
2019-03-05 17:27:18 +0100

    Modify somewhat docker for autotune

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [[ -z "${START_DATE}" ]] || [ "$START_DATE" = "YYYY-MM-DD" ]; then
   echo "START_DATE variable is not set, please do so before using this image"
   exit
@@ -18,16 +19,17 @@ if ! [[ -s /data/profile.json ]]; then
   echo "No profile.json detected, please specify a volume mount for '/data/' and create 'profile.json' in the mount"
   exit
 else
-  cp /data/profile.json /root/myopenaps/settings/profile.json
-  cp /data/profile.json /root/myopenaps/settings/pumpprofile.json
-  cp /data/profile.json /root/myopenaps/settings/autotune.json
+  cp /data/profile.json /openaps/settings/profile.json
+  cp /data/profile.json /openaps/settings/pumpprofile.json
+  cp /data/profile.json /openaps/settings/autotune.json
 fi
 
-oref0-autotune --dir=/root/myopenaps --ns-host="${NS_HOST}" --start-date="${START_DATE}" ${AUTOTUNE_PREFS}
+oref0-autotune --dir=/openaps --ns-host="${NS_HOST}" --start-date="${START_DATE}" ${AUTOTUNE_PREFS}
 
-if ! [[ -s /root/myopenaps/autotune/autotune_recommendations.log ]]; then
+if ! [[ -s /openaps/autotune/autotune_recommendations.log ]]; then
   echo "No recommendations found, perhaps the command failed?"
 else
-  cp /root/myopenaps/autotune/autotune_recommendations.log /data/autotune_recommendations.log
+  cp /openaps/autotune/autotune_recommendations.log /data/autotune_recommendations.log
+  cp /openaps/autotune/profile.json /data/autotune-profile.json
   echo "Recommendations copied to /data/autotune_recommendations.log, check your volume mount"
 fi
